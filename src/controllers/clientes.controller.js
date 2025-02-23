216688a66da8b705bb87e41bf759da90b27db462
juliancgarzon <juliangarzon@live.com>
2025-02-22 21:04:28 -0500

    create funtion getcliente and createclientes

--- src/controllers/clientes.controller.js
+++ src/controllers/clientes.controller.js
@@ -1,19 +1,27 @@
 import {clientes} from "../models/clientes.js";
 
 export const getClientes = async (req, res )=>{
-    const clientesdata = await clientes.findAll()
-    res.json(clientesdata)
+    try {
+        throw new Error('query failed')
+        const clientesdata = await clientes.findAll()
+        res.json(clientesdata);
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+    
 }
 export const createClientes = async (req, res )=>{
-    const {nombre, email, telefono, direccion} = req.body
-    const newclient = await clientes.create ({
-        nombre,
-        email,
-        telefono,
-        direccion
-    })
-
-
-    res.json(newclient)
-}
+    const {nombre, email, telefono, direccion} = req.body;
+    try {
+        const newclient = await clientes.create ({
+            nombre,
+            email,
+            telefono,
+            direccion
+        });
+        res.json(newclient)
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+};
 
