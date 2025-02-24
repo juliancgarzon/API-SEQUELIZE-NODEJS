4fd8d719cda542599af3d34e3ec151c07eed8b04
juliancgarzon <juliangarzon@live.com>
2025-02-24 00:38:27 -0500

    created dockerfiles and create container

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 
 export const sequelize = new Sequelize ('database-api', 'postgres','1234',{
-    host: 'localhost',
+    host: 'node_db',
     dialect:'postgres',
     logging: false,
 });
