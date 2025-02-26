ce1094d4fe55b8f777c56defa7a3a77cfe5636c6
juliancgarzon <juliangarzon@live.com>
2025-02-26 16:41:49 -0500

    modified docker files and databade

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,15 @@
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const sequelize = new Sequelize ('database-api', 'postgres','1234',{
-    host: 'node_db',
-    dialect:'postgres',
+export const sequelize = new Sequelize(
+    process.env.DB_NAME || 'database-api',
+    process.env.DB_USER || 'postgres',
+    process.env.DB_PASSWORD || '1234',
+    {
+    host: process.env.DB_HOST || 'database',
+    dialect: 'postgres',
     logging: false,
-});
+    port: process.env.DB_PORT || 5432,
+    }
+);
