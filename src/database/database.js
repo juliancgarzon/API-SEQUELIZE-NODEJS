d3fa8493f19a17dcfad84401c875afb5d2097fd3
juliancgarzon <juliangarzon@live.com>
2025-02-26 23:10:48 -0500

    conection render posgrest

--- src/database/database.js
+++ src/database/database.js
@@ -2,14 +2,24 @@ import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const sequelize = new Sequelize(
-    process.env.DB_NAME || 'database-api',
-    process.env.DB_USER || 'postgres',
-    process.env.DB_PASSWORD || '1234',
-    {
-    host: process.env.DB_HOST || 'database',
-    dialect: 'postgres',
-    logging: false,
-    port: process.env.DB_PORT || 5432,
-    }
-);
+const DB_URL = process.env.DATABASE_URL || process.env.DB_URL; // 
+
+export const sequelize = DB_URL
+  ? new Sequelize(DB_URL, {
+      dialect: 'postgres',
+      logging: false,
+      dialectOptions: {
+        ssl: { require: true, rejectUnauthorized: false }, // 🔥 Importante para Render
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME || 'database-api',
+      process.env.DB_USER || 'postgres',
+      process.env.DB_PASSWORD || '1234',
+      {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'postgres',
+        logging: false,
+        port: process.env.DB_PORT || 5432,
+      }
+    );
